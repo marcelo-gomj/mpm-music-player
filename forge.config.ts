6593bd8c5e2cf952b510770c412a78c9823ab4e4
Marcelo Gomes <marcelo.gomj@gmail.com>
2023-12-26 09:52:10 -0300

    fix: paths no works

--- forge.config.ts
+++ forge.config.ts
@@ -20,7 +20,7 @@ const config: ForgeConfig = {
           config: 'vite.main.config.ts',
         },
         {
-          entry: ['src/preload/preload.ts'],
+          entry: 'src/preload/preload.ts',
           config: 'vite.preload.config.ts',
         },
       ],
