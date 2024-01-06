560e591dc5a422216f4b7047da42c1ebb59d337d
Marcelo Gomes <marcelo.gomj@gmail.com>
2024-01-06 10:32:30 -0300

    add: million compiler

--- vite.renderer.config.ts
+++ vite.renderer.config.ts
@@ -1,9 +1,11 @@
 import { defineConfig } from 'vite';
+import million from "million/compiler";
 import react from "@vitejs/plugin-react-swc";
 import svgr from "vite-plugin-svgr"
 // https://vitejs.dev/config
 export default defineConfig({
   plugins: [
+    million.vite({}),
     react(),
     svgr()
   ],
